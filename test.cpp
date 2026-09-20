4c63b745bef55d8dccb55d660664f7bb2089d88f
Fishfromthemeditrean <169060445+Fishfromthemeditrean@users.noreply.github.com>
2026-09-20 16:54:59 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 extern "C" { //Stops CPP from obfuscating the func name in the .so 
-    void dosmth(void _NERR) { //NERR makes it so that the godot compilr doesnt crash if it doesn't have any params
+    void dosmth(int _NERR) { //NERR makes it so that the godot compilr doesnt crash if it doesn't have any params
         printf("i did smth\n");
     }
 }
